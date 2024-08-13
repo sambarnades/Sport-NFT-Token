ab14e3a03a853291b7a1967561eeb3e210bd7447
Sam <samuel.barnades@gmail.com>
2024-08-13 09:46:44 +0200

    Add form & start functionality to send the connexion & show the dashboard.

--- src/sport_nft_marketplace_frontend/src/App.jsx
+++ src/sport_nft_marketplace_frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import { sport_nft_marketplace_backend } from 'declarations/sport_nft_marketplace_backend';
+import Navigation from './components/Navigation.jsx';
+
+function connexionSubmit() {
+  let identifier = document.getElementById('identifier').value.toString();
+  console.log(identifier);
+
+
+}
 
 function App() {
 
 
   return (
-    <main>
-      
+    <main className='container-fluid'>
+     <Navigation />
     </main>
   );
 }
--- src/sport_nft_marketplace_frontend/src/main.jsx
+++ src/sport_nft_marketplace_frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/js/index.umd.js';
+import 'bootstrap-icons/font/bootstrap-icons.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
